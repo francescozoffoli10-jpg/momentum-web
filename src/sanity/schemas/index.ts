b6d678f8e4791195a8018ca8481946b5804f7984
francescozoffoli10-jpg <francescozoffoli10@gmail.com>
2026-06-01 19:02:47 +0200

    feat: register teatroShow schema

--- src/sanity/schemas/index.ts
+++ src/sanity/schemas/index.ts
@@ -1,4 +1,5 @@
 import { schemas as tenantSchemas } from './tenant'
 import { siteEvent } from './siteEvent'
+import { teatroShow } from './teatroShow'
 
-export const schemaTypes = [...tenantSchemas, siteEvent]
+export const schemaTypes = [...tenantSchemas, siteEvent, teatroShow]
